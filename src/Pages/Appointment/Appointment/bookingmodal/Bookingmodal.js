e38772c1487ec762a2dcb7a4a4b87aa30c9caf81
Md Shakil monsi <samdshakilofficial@gmail.com>
2023-09-28 11:57:45 +0600

    add onsubmit and data shes hole desibuld button

--- src/Pages/Appointment/Appointment/bookingmodal/Bookingmodal.js
+++ src/Pages/Appointment/Appointment/bookingmodal/Bookingmodal.js
@@ -2,8 +2,19 @@ import { format } from 'date-fns';
 import React from 'react';
 
 const Bookingmodal = ({treatment,selectedDate}) => {
+    
     const date = format(selectedDate,'PP')
     const {name ,slots}=treatment //treatment and appointment option 
+
+    const handleBooking =even=>{
+        even.preventDefault()
+        const form =even.target 
+        const slot= form.slot.value 
+        const name= form.name.value 
+        const email= form.email.value
+        const phone= form.phone.value
+        console.log(slot,name,email,phone)
+    }
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -12,9 +23,9 @@ const Bookingmodal = ({treatment,selectedDate}) => {
  <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 ">X</label>
 
 <h3 className='text-lg font-bold '>{name}</h3>
-<form className='grid grid-cols-1 gap-3 mt-10 '>
+<form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10 '>
 <input type="text" disabled value={date} className="input w-full input-bordered  " />
-<select className="select select-bordered w-full">
+<select name='slot' className="select select-bordered w-full">
  {
 
    slots?.map(slot=><option value={slot}>{slot}</option>)
@@ -22,9 +33,9 @@ const Bookingmodal = ({treatment,selectedDate}) => {
  }
 
 </select>
-<input type="text" placeholder="Type here" className="input w-full input-bordered " />
-<input type="text" placeholder="Type here" className="input w-full input-bordered " />
-<input type="text" placeholder="Type here" className="input w-full only:input-bordered " />
+<input type="text" name='name' placeholder="your Name" className="input w-full input-bordered " />
+<input type="email" name='email' placeholder=" your Email" className="input w-full input-bordered " />
+<input type="text" name='phone' placeholder="your phone" className="input w-full only:input-bordered " />
 <br/>
 <input className='btn btn-accent w-full' type='submit ' value="submit"/>
 
