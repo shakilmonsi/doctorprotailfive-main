f52b9bf50c2c0f154b1dd16e096426bb4bb9001a
Md Shakil monsi <samdshakilofficial@gmail.com>
2023-09-30 20:24:21 +0600

    authprovider a setLoading setup

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -6,24 +6,30 @@ const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
 const [user,setUser]= useState(null)
+const [loading ,setLoading]= useState(true)
+
 
 
     const createUser = (email,password)=>{
+      setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password)
     }
     const signIn = (email, password) =>{
+      setLoading(tru)
       return signInWithEmailAndPassword(auth, email, password);
   }
   const logOut = ()=>{
     return signOut(auth)
   }
   const updateUser =(userInfo)=>{
+    setLoading(true)
     return updateProfile(auth,userInfo)
   }
   useEffect(()=>{
  const unsubscripe=   onAuthStateChanged(auth,currentUser=>{
       console.log('user opserviing ')
       setUser(currentUser)
+      setLoading(false)
     })
     return()=>unsubscripe()
 
